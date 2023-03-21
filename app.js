81e0df9bb356c65765c1c5d51eb62ef45e694591
baggy102 <rlgh0806@gmail.com>
2023-03-21 19:03:12 +0900

    올려드립니다

--- app.js
+++ app.js
@@ -3,34 +3,34 @@ const app = express();
 //static & views 설정
 const port = 8000;
 
-app.set("view engine", "ejs");
-app.set("/views", "views");
+app.set('view engine', 'ejs');
+app.set('/views', 'views');
 
-app.use("/static", express.static(__dirname + "/static"));
+app.use('/static', express.static(__dirname + '/static'));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const area = req.params.area;
-  res.render("index");
-
+  res.render('index');
 });
 
-app.get('/second', (req, res) => {
+app.get('/yongsan', (req, res) => {
   res.render('yongsan');
+});
 
-app.get("/service_info", (req, res) => {
-  res.render("service_info");
+app.get('/service_info', (req, res) => {
+  res.render('service_info');
 });
-app.get("/faq", (req, res) => {
-  res.render("faq");
+app.get('/faq', (req, res) => {
+  res.render('faq');
 });
-app.get("/customer_service", (req, res) => {
-  res.render("customer_service");
+app.get('/customer_service', (req, res) => {
+  res.render('customer_service');
 });
-app.get("/customer_service/suggestion", (req, res) => {
-  res.render("suggestion");
+app.get('/customer_service/suggestion', (req, res) => {
+  res.render('suggestion');
 });
-app.get("/customer_service/inquiry", (req, res) => {
-  res.render("inquiry");
+app.get('/customer_service/inquiry', (req, res) => {
+  res.render('inquiry');
 });
 
 app.listen(port, () => {
--- static/js/second.js
+++ static/js/second.js
@@ -1,10 +1,10 @@
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 2,
   autoplay: true,
   mousewheel: true,
-  speed: 1000,
-  spaceBetween: 10,
+  speed: 2000,
+  spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
